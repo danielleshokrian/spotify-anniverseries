b04b6124f66863c86b02de9cca366165c6874a63
Danielle <danielle.shokrian@aol.com>
2025-10-07 12:29:31 -0400

    updating theme to black, no purple

--- music-anniversary-finder/src/components/Layout.jsx
+++ music-anniversary-finder/src/components/Layout.jsx
@@ -4,9 +4,14 @@ import Navbar from './Navbar';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-purple-900 via-purple-800 to-indigo-900">
+    <div className="min-h-screen bg-black relative overflow-hidden">
+      <div className="absolute inset-0 opacity-5">
+        <div className="absolute top-20 left-10 w-96 h-96 rounded-full border-[40px] border-gray-800"></div>
+        <div className="absolute bottom-20 right-10 w-96 h-96 rounded-full border-[40px] border-gray-800"></div>
+      </div>
+      
       <Navbar />
-      <main>
+      <main className="relative z-10">
         <Outlet />
       </main>
     </div>
